60ddd78c6ac1bc183705b6bf441d79e929bb99c6
James Hay <pokinoice@live.com>
2018-05-11 23:25:30 +0800

    Delete example

--- src/example.js
+++
@@ -1,6 +0,0 @@
-const Baboom = require('./index');
-
-let newE = Baboom.badRequest({ test: 'value' });
-
-console.log('Outgoing payload', newE.output.payload);
-console.log('Is Boom', Baboom.isBoom(newE));
